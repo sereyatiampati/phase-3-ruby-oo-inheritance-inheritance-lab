16155b969d6415437e35f01ede5d711760572aa6
Sereya Tiampati <sereyatiampatistudies@gmail.com>
2023-02-14 08:05:40 +0300

    done

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,21 @@
-class Student 
+require_relative './user'
+class Student < User
+    # @student_knowledge=Array.new
+    attr_accessor :knowledge
+    def initialize(knowledge=Array.new)
+        @knowledge=knowledge
+    end
 
+    def learn(str)
+        @knowledge << str
+    end
+
+    def knowledge
+        @knowledge
+    end
 end
+serena= Student.new
+puts serena.first_name= "Serena"
+puts serena.last_name= "Sereya"
+pp serena.learn("Best student in Math")
+pp serena .knowledge
